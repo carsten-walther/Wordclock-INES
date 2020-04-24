3079127afef3a31ddff1c8246da5bc9a5f8df6f2
Carsten Walther <carsten.walther@599media.de>
2020-04-24 09:08:14 +0200

    clean up; fix timezone.php for select generation; add select to html template

--- Firmware/helper/timezones.php
+++ Firmware/helper/timezones.php
@@ -18,24 +18,28 @@ foreach ($regions as $name => $mask) {
 
     $zones = DateTimeZone::listIdentifiers($mask);
 
-    foreach($zones as $timezone) {
-		// Lets sample the time there right now
-		$time = new DateTime(NULL, new DateTimeZone($timezone));
-		// Us dumb Americans can't handle millitary time
-		$ampm = $time->format('H') > 12 ? ' ('. $time->format('g:i a'). ')' : '';
-		// Remove region name and add a sample time
-		$timezones[$name][$timezone] = substr($timezone, strlen($name) + 1) . ' - ' . $time->format('H:i') . $ampm;
-	}
+    foreach ($zones as $timezone) {
+        $tz = new DateTimeZone($timezone);
+        // Lets sample the time there right now
+        $time = new DateTime(NULL, $tz);
+        // Us dumb Americans can't handle millitary time
+        $ampm = $time->format('H') > 12 ? ' ('. $time->format('g:i a'). ')' : '';
+        // Remove region name and add a sample time
+        $timezones[$name][$timezone] = [
+            'offset' => ($time->format('Z') / 3600),
+            'label' => substr($timezone, strlen($name) + 1) . ' - ' . $time->format('H:i') . $ampm
+        ];
+    }
 }
 
 // View
 
-print '<label>Select Your Timezone</label><select id="timezone">';
-foreach($timezones as $region => $list) {
-	print '<optgroup label="' . $region . '">' . "\n";
-	foreach($list as $timezone => $name) {
-		print '<option name="' . $timezone . '">' . $name . '</option>' . "\n";
-	}
-	print '<optgroup>' . "\n";
+print '<select id="timezone">';
+foreach ($timezones as $region => $listItems) {
+    print '<optgroup label="' . $region . '">';
+    foreach ($listItems as $name => $listItem) {
+        print '<option value="' . $listItem['offset'] . '" name="' . $name . '">' . $listItem['label'] . '</option>';
+    }
+    print '</optgroup>';
 }
 print '</select>';
